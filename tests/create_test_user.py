1dc132e1041e12272d77261dd917d4b2296461bb
nogaems <nogaems@users.noreply.github.com>
2019-06-15 00:57:23 +0300

    add: tests for /v1/api/login

---
+++ tests/create_test_user.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+
+# a quite bit of pure suffering here
+import os
+import sys
+
+src = 'src'
+path = os.path.dirname(os.path.realpath(
+    os.path.join(os.getcwd(), os.path.expanduser(__file__))))
+parent = os.path.normpath(os.path.join(path, '..'))
+src = os.path.join(parent, src)
+
+os.chdir(src)
+sys.path.append(src)
+
+# now we're able to use our modules normally
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import create_engine
+
+
+import model
+import config
+
+engine = create_engine(config.db_uri, echo=config.sql_debug)
+Session = sessionmaker(bind=engine)
+session = Session()
+user = session.query(model.User).filter_by(username='test').first()
+if not user:
+    session.add(model.User('test', 'test'))
+    session.commit()
